c50f899577fbcd415100bd385491c95b3a53f9d9
Aaroh Purani <aaroskey@gmail.com>
2026-05-18 23:28:46 +1000

    Made a basic profile creation/editing page. Nothing being stored rn so it's really just the ui in theory. Things can be changed around later.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,22 +1,18 @@
-import styles from "./page.module.css";
-import { createClient } from '../utils/supabase/server'
-import { cookies } from 'next/headers'
 import Navbar from "@/components/navbar";
+import { createClient } from "../utils/supabase/server";
+import { cookies } from "next/headers";
+import ProfileEditor from "./profile-editor";
 
 export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
-  const { data } = await supabase.from('data').select()
+  const { data } = await supabase.from("data").select();
 
   return (
     <>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      <ProfileEditor initialData={data ?? []} />
       <Navbar />
     </>
-  )
+  );
 }
