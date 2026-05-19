6a52ab809eadf3451e1dd35c4e8a323fe1da39e1
oscar <222574796+raxzy0@users.noreply.github.com>
2026-05-19 12:18:54 +1000

    added profile

--- frontend/src/app/matching/page.js
+++ frontend/src/app/matching/page.js
@@ -5,7 +5,7 @@ import { createClient } from '@/utils/supabase/server'
 export default async function Matching() {
   const supabase = await createClient()
 
-  const { data } = await supabase.from('data').select()
+  const { data } = await supabase.from('profiles').select()
 
   return (
     <>
